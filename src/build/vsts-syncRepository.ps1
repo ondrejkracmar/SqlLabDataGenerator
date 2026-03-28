5892f4efb2cccdfdfc1acf755bb945dfd9c28c5a
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2026-03-28 09:19:37 +0100

    Fix issuew

--- src/build/vsts-syncRepository.ps1
+++ src/build/vsts-syncRepository.ps1
@@ -9,21 +9,23 @@ param (
 	[string]$GitHubToken
 )
 
+$ErrorActionPreference = 'Stop'
+
 try {
 	# Construct the Azure DevOps and GitHub repository URLs
-	$encodedAzureDevOpsPAT = [System.Web.HttpUtility]::UrlEncode($AzureDevOpsToken)
-	$encodedGitHubToken = [System.Web.HttpUtility]::UrlEncode($GitHubToken)
+	$encodedAzureDevOpsPAT = [uri]::EscapeDataString($AzureDevOpsToken)
+	$encodedGitHubToken = [uri]::EscapeDataString($GitHubToken)
 	$azureRepoUrl = ('https://{0}@dev.azure.com/{1}/{2}/_git/{3}' -f $encodedAzureDevOpsPAT, $AzureDevOpsOrganizationName, $AzureDevOpsProjectName, $AzureDevOpsRepositoryName)
 	$gitHubRepoUrl = ('https://{0}:{1}@github.com/{2}/{3}' -f $GitHubUsername, $encodedGitHubToken, $GitHubUsername, $GitHubRepositoryName)
 
-	# Configure Git to use credentials for Azure DevOps
+	# Configure Git to use credentials for Azure DevOps (double-quoted so variables expand)
 	Write-Host "Configuring Git credentials for Azure DevOps..."
-	$credentialAzureDevOpsContent = @'
+	$credentialAzureDevOpsContent = @"
 protocol=https
 host=dev.azure.com
-username=$AzureDevOpsUserName
+username=$AzureDevOpsUsername
 password=$AzureDevOpsToken
-'@
+"@
 
 	$tempCredentialFile = New-TemporaryFile
 	$credentialAzureDevOpsContent | Set-Content -Path $tempCredentialFile.FullName
@@ -37,6 +39,9 @@ password=$AzureDevOpsToken
 	# Clone the Azure DevOps repository in mirror mode
 	Write-Host "Cloning Azure DevOps repository..."
 	git clone --mirror $azureRepoUrl repo.git
+	if ($LASTEXITCODE -ne 0) { throw "git clone failed with exit code $LASTEXITCODE" }
+
+	Set-Location repo.git
 
 	# Add GitHub as a remote repository
 	Write-Host "Adding GitHub remote repository..."
@@ -46,6 +51,7 @@ password=$AzureDevOpsToken
 	# Push to GitHub
 	Write-Host "Pushing to GitHub..."
 	git push --mirror github
+	if ($LASTEXITCODE -ne 0) { throw "git push failed with exit code $LASTEXITCODE" }
 
 	# Return to the original directory
 	Set-Location -Path ..
